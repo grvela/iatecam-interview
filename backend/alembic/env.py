80a442f20c7e39a70c42161aed24f8f772cddc38
rodrigo vela <rodrigoandre777@gmail.com>
2023-10-06 19:59:03 -0400

    fix(migrations): adding new infos to tables

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -69,7 +69,7 @@ def run_migrations_online() -> None:
 
     """
 
-    from app.database.models import user, input, output, storage, product, tag
+    from app.models import user, input, output, storage, product, tag
 
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
